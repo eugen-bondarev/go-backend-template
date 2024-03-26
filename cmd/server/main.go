b9b6212103d561905e88da716559ef1791a06747
Eugen Bondarev <eug.bondarev@gmail.com>
2024-03-26 10:41:44 +0000

    wip

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"go-backend-template/internal/impl"
+	"go-backend-template/internal/middleware"
 	"go-backend-template/internal/model"
 	"go-backend-template/internal/util"
 	"os"
@@ -12,10 +13,25 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type Permission struct {
+	Object  string
+	Action  string
+	Subject string
+}
+
+func NewPermission(subject, action, object string) Permission {
+	return Permission{
+		Subject: subject,
+		Action:  action,
+		Object:  object,
+	}
+}
+
 type App struct {
-	userRepo   model.UserRepo
-	signingSvc model.SigningSvc
-	authSvc    model.AuthSvc
+	userRepo    model.UserRepo
+	signingSvc  model.SigningSvc
+	authSvc     model.AuthSvc
+	permissions []Permission
 }
 
 func NewApp() (App, error) {
@@ -37,53 +53,52 @@ func NewApp() (App, error) {
 	}
 
 	userRepo := impl.NewPGUserRepo(&pg)
-
-	if err != nil {
-		return App{}, err
-	}
-
 	authSvc := impl.NewDefaultAuthSvc(userRepo, "foobar")
-	// err = authSvc.CreateUser("admin@example.com", "lorem ipsum", "admin")
-
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return
-	// }
-
-	user, err := authSvc.AuthenticateUser("admin@example.com", "lorem ipsum")
-
-	if err != nil {
-		return App{}, err
-	}
-
-	fmt.Println("Successfully authenticated", user)
-
 	signingSvc := impl.NewJWTSigningSvc("foo")
-	token, err := signingSvc.Sign(user.ID, user.Role)
 
-	if err != nil {
-		return App{}, err
-	}
-
-	fmt.Println("Successfully signed", token)
-
-	parsedID, parsedRole, err := signingSvc.Parse(token)
-
-	if err != nil {
-		return App{}, err
-	}
-
-	fmt.Println("Successfully parsed", parsedID, parsedRole)
+	permissions := make([]Permission, 0)
+	permissions = append(permissions, NewPermission("user", "list", "users"))
+	permissions = append(permissions, NewPermission("admin", "list", "users"))
 
 	return App{
-		userRepo:   userRepo,
-		signingSvc: signingSvc,
-		authSvc:    authSvc,
+		userRepo:    userRepo,
+		signingSvc:  signingSvc,
+		authSvc:     authSvc,
+		permissions: permissions,
 	}, nil
 }
 
 func (app *App) users() ([]model.User, error) {
 	return app.userRepo.GetUsers()
+}
+
+func (app *App) authMiddleware(ctx *gin.Context) {
+	authHeader := ctx.Request.Header.Get("Authorization")
+	middleware.Auth(
+		app.signingSvc,
+		authHeader,
+		func(ID int, role string) {
+			ctx.Set("ID", ID)
+			ctx.Set("role", role)
+		},
+	)
+}
+
+func (app *App) requiredAuthMiddleware(action, object string) func(*gin.Context) error {
+	return func(ctx *gin.Context) error {
+		role, _ := ctx.Get("role")
+
+		for _, permission := range app.permissions {
+			if permission.Subject == role && permission.Action == action && permission.Object == object {
+				return nil
+			}
+		}
+
+		return &util.RequestError{
+			StatusCode: 403,
+			Err:        fmt.Errorf("unauthorized"),
+		}
+	}
 }
 
 func main() {
@@ -101,9 +116,15 @@ func main() {
 
 	r := gin.Default()
 	v1 := r.Group("/v1")
-	v1.GET("/users", util.DecorateHandler(func(ctx *gin.Context) (any, error) {
-		return app.users()
-	}))
+
+	v1.GET(
+		"/users",
+		util.DecorateMiddleware(app.authMiddleware),
+		util.DecorateRequiredMiddleware(app.requiredAuthMiddleware("list", "users")),
+		util.DecorateHandler(func(ctx *gin.Context) (any, error) {
+			return app.users()
+		}),
+	)
 
 	auth := v1.Group("/auth")
 
@@ -126,8 +147,7 @@ func main() {
 		var payload Payload
 		ctx.ShouldBindBodyWith(&payload, binding.JSON)
 
-		err := app.register(payload.Email, payload.Password)
-		return nil, err
+		return nil, app.register(payload.Email, payload.Password)
 	}))
 
 	r.Run("0.0.0.0:8081")
--- internal/util/gin.go
+++ internal/util/gin.go
@@ -26,12 +26,45 @@ func GetParamInt(params *gin.Params, key string) (int, bool) {
 	return converted, true
 }
 
+func DecorateMiddleware(handler func(*gin.Context)) func(*gin.Context) {
+	return func(ctx *gin.Context) {
+		handler(ctx)
+		ctx.Next()
+	}
+}
+
+func DecorateRequiredMiddleware(handler func(*gin.Context) error) func(*gin.Context) {
+	return func(ctx *gin.Context) {
+		err := handler(ctx)
+
+		if err == nil {
+			ctx.Next()
+			return
+		}
+
+		parsedErr, ok := err.(*RequestError)
+		if ok {
+			ctx.JSON(parsedErr.StatusCode, gin.H{
+				"error": parsedErr.Err.Error(),
+			})
+			ctx.Abort()
+			return
+		}
+
+		ctx.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		ctx.Abort()
+	}
+}
+
 func DecorateHandler(handler func(*gin.Context) (any, error)) func(*gin.Context) {
 	return func(ctx *gin.Context) {
 		result, err := handler(ctx)
 
 		if err != nil {
 			parsedErr, ok := err.(*RequestError)
+			ctx.Header("Content-Type", "application/problem+json")
 			if ok {
 				ctx.JSON(parsedErr.StatusCode, gin.H{
 					"error": parsedErr.Err.Error(),
